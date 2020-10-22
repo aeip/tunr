c18d136e46a7278804879e3b24873f07cb5c0937
Zane <zanebgriffin@gmail.com>
2020-10-22 11:11:49 -0500

    updated server

--- server.js
+++ server.js
@@ -3,4 +3,19 @@ const express = require('express')
 const {PORT} = process.env
 const cors = require('cors')
 const corsOptions = require('./config/cors')
-const 
+const {NODE_ENV} = process.env
+const morgan = require('morgan')
+const app = express()
+
+
+NODE_ENV === 'production' ? app.use(cors(corsOptions)) : app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.json({hello: "Hello World!"})
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on port:', PORT)
+})
